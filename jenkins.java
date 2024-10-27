aeb20c5c1037c61397aa2b08af2fc310e1be2159
abhinashwere1994 <abhinashwere1994@gmail.com>
2024-10-27 15:54:32 +0530

    Update jenkins.java

--- jenkins.java
+++ jenkins.java
@@ -2,6 +2,8 @@
     public static void main(String[] args) {
         System.out.println("this is print here number line");
         System.out.println("welcome");
+       System.out.println("wel");
+     
         
     }
 
